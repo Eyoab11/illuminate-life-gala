244dc006604365e426c6fb47f42970194e515dc0
Eyoab <eyoabamare3@gmail.com>
2026-05-14 18:49:42 +0300

    fix: selected one section and used another image

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@ import Navigation from '@/components/Navigation';
 import Hero from '@/components/Hero';
 import Countdown from '@/components/Countdown';
 import About from '@/components/About';
-import DrErsnoSection1 from '@/components/DrErsnoSection1';
 import Programs from '@/components/Programs';
 import Experience from '@/components/Experience';
-import DrErsnoSection2 from '@/components/DrErsnoSection2';
 import Tickets from '@/components/Tickets';
 import Sponsors from '@/components/Sponsors';
 import DrErsnoSection3 from '@/components/DrErsnoSection3';
@@ -24,10 +22,8 @@ export default function Home() {
       <Hero />
       <Countdown />
       <About />
-      <DrErsnoSection1 />
       <Programs />
       <Experience />
-      <DrErsnoSection2 />
       <Tickets />
       <Sponsors />
       <DrErsnoSection3 />
